ccc2b3b179e0c0872f4ca883a4b4f9823f6de1c9
Kevin Gilpin <kgilpin@gmail.com>
2015-06-09 18:04:25 -0400

    obtain login and api key by substitution; run webapp and backend on different host ports

--- examples/listy/build.sh
+++ examples/listy/build.sh
@@ -8,7 +8,7 @@ $generate global     sdf.yml > gatekeeper/nginx/conf/global.conf
 $generate gatekeeper sdf.yml > gatekeeper/nginx/conf/gate.conf
 
 $generate global    sdf.yml > backend/nginx/conf/global.conf
-$generate forwarder sdf.yml > backend/nginx/conf/forward.conf
+$generate forwarder sdf.yml > backend/nginx/conf/forward.conf.template
 
 docker-compose build
 cd -
--- sdf-gen.gemspec
+++ sdf-gen.gemspec
@@ -21,6 +21,7 @@ Gem::Specification.new do |spec|
   spec.add_dependency "gli"
   spec.add_dependency "hashr"
 
+  spec.add_development_dependency "conjur-cli"
   spec.add_development_dependency "bundler", "~> 1.7"
   spec.add_development_dependency "rake", "~> 10.0"
 end
