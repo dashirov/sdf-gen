012931b076bd9e0cff9dad9f24d2ab3d5f4cf03d
Kevin Gilpin <kgilpin@gmail.com>
2015-06-10 17:28:57 -0400

    update generated files in asgard example

--- examples/asgard/build.sh
+++ examples/asgard/build.sh
@@ -3,11 +3,9 @@
 mkdir -p generated/asgard
 mkdir -p generated/eureka
 
-generate="bundle exec ../../bin/sdf-gen"
+sdf-gen global     asgard.yml > generated/asgard/global.conf
+sdf-gen gatekeeper asgard.yml > generated/asgard/gate.conf
+sdf-gen forwarder  asgard.yml > generated/asgard/forward-eureka.conf
 
-$generate global     asgard.yml > generated/asgard/global.conf
-$generate gatekeeper asgard.yml > generated/asgard/gate.conf
-$generate forwarder  asgard.yml > generated/asgard/forward-eureka.conf
-
-$generate global     eureka.yml > generated/eureka/global.conf
-$generate gatekeeper eureka.yml > generated/eureka/gate.conf
+sdf-gen global     eureka.yml > generated/eureka/global.conf
+sdf-gen gatekeeper eureka.yml > generated/eureka/gate.conf
