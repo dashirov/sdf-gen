c26cf2df46407435b8001daaed5be60f5e660790
Kevin Gilpin <kgilpin@gmail.com>
2015-06-10 11:53:26 -0400

    remove unused Gemfile stuff; sdf.yml will be generated so that the appliance url, account name and policy are correct

--- examples/listy/build.sh
+++ examples/listy/build.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-generate="bundle exec ../../../bin/sdf-gen"
+ruby sdf.rb
 
 cd webapp
 
-$generate global     sdf.yml > gatekeeper/nginx/conf/global.conf
-$generate gatekeeper sdf.yml > gatekeeper/nginx/conf/gate.conf
+sdf-gen global     sdf.yml > gatekeeper/nginx/conf/global.conf
+sdf-gen gatekeeper sdf.yml > gatekeeper/nginx/conf/gate.conf
 
-$generate global    sdf.yml > backend/nginx/conf/global.conf
-$generate forwarder sdf.yml > backend/nginx/conf/forward.conf.template
+sdf-gen global    sdf.yml > backend/nginx/conf/global.conf
+sdf-gen forwarder sdf.yml > backend/nginx/conf/forward.conf.template
 
 docker-compose build
 cd -
 
 cd backend
-$generate global     sdf.yml > gatekeeper/nginx/conf/global.conf
-$generate gatekeeper sdf.yml > gatekeeper/nginx/conf/gate.conf
+sdf-gen global     sdf.yml > gatekeeper/nginx/conf/global.conf
+sdf-gen gatekeeper sdf.yml > gatekeeper/nginx/conf/gate.conf
 
 docker-compose build
 cd -
---
+++ examples/listy/sdf.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'conjur/cli'
+require 'uri'
+require 'json'
+
+Conjur::Config.load
+Conjur::Config.apply
+
+policy = JSON.load(File.read(File.expand_path('../policy-sandbox.json', __FILE__)))
+policy_id = policy['policy']
+hostname = URI.parse(Conjur.configuration.appliance_url).host
+
+File.write File.expand_path('../backend/sdf.yml', __FILE__), <<SDF
+global:
+  conjur:
+    hostname: #{hostname}
+
+gatekeeper:
+  port: 80
+  service: service:4567
+  conjur_account:  #{Conjur.configuration.account}
+  conjur_resource: webservice/#{policy_id}/backend
+SDF
+
+File.write File.expand_path('../webapp/sdf.yml', __FILE__), <<SDF
+global:
+  conjur:
+hostname: #{hostname}
+
+gatekeeper:
+  port: 80
+  service: service:80
+  conjur_account:  #{Conjur.configuration.account}
+  conjur_resource: webservice/#{policy_id}/backend
+
+forward:
+  backend:
+    listen: localhost
+    service: localhost:8085
+SDF
